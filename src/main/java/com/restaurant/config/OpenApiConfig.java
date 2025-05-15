c40f45bb7a8d90f8993adbc17c645b3a17b8aea9
AVargass19 <u20231213646@usco.edu.co>
2025-05-15 11:11:22 -0500

    Configuracion de OpenApi

---
+++ src/main/java/com/restaurant/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package com.restaurant.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Sistema de Reservas de Restaurante")
+                        .version("1.0")
+                        .description("API para la gestión de reservas de un restaurante")
+                        .contact(new Contact()
+                                .name("Ana Sofía Vargas")
+                                .email("u20231213646@usco.edu.co")
+                                .url("https://www.usco.edu.co/")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth",
+                                        new SecurityScheme()
+                                                .name("bearerAuth")
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
+}
